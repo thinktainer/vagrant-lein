ff1a202c27befda113bc4b0e5e134d19e29d63b4
Martin Schinz <m.schinz@gmail.com>
2014-08-19 18:38:24 +0100

    whitespace

--- modules/dev/manifests/lein.pp
+++ modules/dev/manifests/lein.pp
@@ -1,21 +1,25 @@
 class dev::lein {
+
   file { '/home/vagrant/bin':
     ensure => directory,
     owner => "vagrant",
     group => "vagrant",
     mode => 0750
   }
+
   exec { 'retrieve_leiningen':
     command => "/usr/bin/wget -q https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O /home/vagrant/bin/lein",
     creates => "/home/vagrant/bin/lein",
     require => File["/home/vagrant/bin"]
   }
+
   file { '/home/vagrant/bin/lein':
     mode => 0755,
     owner => "vagrant",
     group => "vagrant",
     require => Exec["retrieve_leiningen"]
   }
+
   exec {'run_lein':
     command => "lein",
     user => "vagrant",
@@ -26,4 +30,5 @@ class dev::lein {
     creates => "/home/vagrant/.lein",
     require => File["/home/vagrant/bin/lein"]
   }
+
 }
